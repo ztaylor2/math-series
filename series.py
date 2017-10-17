826b3b3ec529bcd0112e6e2b714fdf9d66665a49
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-17 14:51:12 -0700

    built out fib and lucas

--- series.py
+++ series.py
@@ -1,3 +1,31 @@
 """401d7 day 2 lab assignment. Mike Shinners & Zach Taylor."""
 
 
+def fibonacci(n):
+    """creates a fibonacci series"""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def lucas(n):
+    """creates a lucas series"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, first=0, second=1):
+    """creates a lucas series"""
+    if n == 0:
+        return first
+    elif n == 1:
+        return second
+    else:
+        return lucas(n - 1) + lucas(n - 2)
---
+++ test_series.py
@@ -0,0 +1,19 @@
+import pytest
+
+FIB_TABLE = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 8)]
+
+LUC_TABLE = [(0, 2), (1, 1), (2, 3), (3, 4), (4, 7), (5, 11), (6, 18)]
+
+
+@pytest.mark.parametrize('n, result', FIB_TABLE)
+def test_fibonacci(n, result):
+    """testing our fibonacci() funciton"""
+    from series import fibonacci
+    assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize('n, result', LUC_TABLE)
+def test_lucas(n, result):
+    """testing our lucas function"""
+    from series import lucas
+    assert lucas(n) == result
